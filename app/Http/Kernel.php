a7d77f5bbb9cc4a2c09f90b95590231e5d4e76fe
rohit-mouni <rohitmounioo7@gmail.com>
2024-07-01 12:35:06 +0530

    create admin guard

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'not.logged.in' => \App\Http\Middleware\NotLoggedIn::class,
+        'Authenticated' => \App\Http\Middleware\Authentication::class,
     ];
 }
