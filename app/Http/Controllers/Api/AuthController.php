131724a0a9491e870bf8d3c52688c2f50d34c1ab
surendra singh panwar <ssptech8@gmail.com>
2024-07-01 13:29:43 +0530

    small change in api controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -45,30 +45,40 @@ class AuthController extends Controller
     }
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'brand_name' => 'required|string|min:3',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:8|confirmed',
-        ]);
+        $user_type = $request->user_type;
 
-        if ($validator->fails()) {
+        if($user_type == "brand")
+        {
+            $validator = Validator::make($request->all(), [
+                'brand_name' => 'required|string|min:3',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|min:8|confirmed',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    "status" => "error",
+                    "message" => $validator->errors()->first()
+                ], 422);
+            }
+
+            $user = new User;
+            $user->brand_name = $request->brand_name;
+            $user->email = $request->email;
+            $user->user_type = 'brand';
+            $user->password = Hash::make($request->password);
+            $user->save();
+
             return response()->json([
-                "status" => "error",
-                "message" => $validator->errors()->first()
-            ], 422);
+                "status" => "success",
+                "message" => "brand registered successfully"
+            ]);
         }
-
-        $user = new User;
-        $user->brand_name = $request->brand_name;
-        $user->email = $request->email;
-        $user->user_type = 'brand';
-        $user->password = Hash::make($request->password);
-        $user->save();
-
-        return response()->json([
-            "status" => "success",
-            "message" => "brand registered successfully"
-        ]);
+        else
+        if($user_type == "creator")
+        {
+            dd("register creator...");
+        }
     }
     public function logout()
     {
